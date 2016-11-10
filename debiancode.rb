0734a7a1ef8a1de231ef446dec10aa0208fd336e
kd1240961 <kd1240961@st.kobedenshi.ac.jp>
2016-11-10 03:52:12 +0000

    テーブル表示

--- debiancode.rb
+++ debiancode.rb
@@ -7,6 +7,11 @@ require 'nokogiri'
 url = 'https://wiki.debian.org/DebianReleases'
 html = open(url).read
 
-puts html
-# doc = Nokogiri::HTML.parse(html)
-# pp doc
+# puts html
+doc = Nokogiri::HTML.parse(html)
+doc.at('//tbody').xpath('.//tr').each do |e|
+  e.xpath('.//td').each do |f|
+    print f.inner_text.ljust(25)
+  end
+  puts
+end
